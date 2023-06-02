f4b76a8641115c0def63eee1cdc5b9a1bed12c96
Dacili <diname_girl@hotmail.com>
2023-06-02 15:30:44 +0200

    adding db things, + service

---
+++ GraphQLwithReact/GraphQLwithReact/Entities/Masjid.cs
@@ -0,0 +1,14 @@
+﻿namespace GraphQLwithReact.Entities
+{
+    public class Masjid
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public double? SizeSqm { get; set; }
+        public int? CapacityOfPeople { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
+    }
+}
---
+++ GraphQLwithReact/GraphQLwithReact/Repositories/MasjidService.cs
@@ -0,0 +1,63 @@
+﻿using GraphQLwithReact.Data;
+using GraphQLwithReact.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraphQLwithReact.Repositories
+{
+    public class MasjidService: IMasjidService
+    {
+        private readonly DbContextMediClass dbContextMedi;
+        public MasjidService(DbContextMediClass dbContextMedi)
+        {
+            this.dbContextMedi = dbContextMedi;
+        }
+
+        public async Task<List<Masjid>> GetAllMasjidsAsync()
+        {
+          return await  dbContextMedi.Masjids.ToListAsync();
+        }
+
+        public async Task<Masjid> GetMasjidByIdAsync(Guid Id)
+        {
+            return await dbContextMedi.Masjids.Where(masjid => masjid.Id == Id).FirstOrDefaultAsync();
+        }
+
+        public async Task<Masjid> CreateMasjidAsync(Masjid masjid)
+        {
+            await dbContextMedi.Masjids.AddAsync(masjid);
+            //var res =
+            await dbContextMedi.SaveChangesAsync();
+            return masjid;
+
+        }
+
+        public async Task<Masjid> UpdateMasjidAsync(Masjid masjid)
+        {
+            if(dbContextMedi.Masjids.Where(masjid1 => masjid1.Id == masjid.Id).FirstOrDefault() != null) { 
+            dbContextMedi.Masjids.Update(masjid);
+           //var res =
+                await dbContextMedi.SaveChangesAsync();
+            }
+            return masjid;
+            //if (res > 0)
+            //{
+            //    return true;
+            //}
+            //else return false;
+        }
+
+        public async Task<Masjid> DeleteMasjidAsync(Guid id)
+        {
+            var masjid = dbContextMedi.Masjids.Where(masjid => masjid.Id == id).FirstOrDefault();
+        if(masjid != null)
+            {
+              dbContextMedi.Masjids.Remove(masjid);
+              var res=  await dbContextMedi.SaveChangesAsync();
+                
+                //if (res > 0) return true;
+                //return false;
+            }
+            return masjid;
+        }    
+    }
+}
